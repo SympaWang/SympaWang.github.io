d1c6a71816b193486ad3aabafe1c1c3fde7bc0a9
Your Name <you@example.com>
2020-06-06 18:05:33 +0800

    Site updated: 2020-06-06 18:05:33

--- dist/music.js
+++ dist/music.js
@@ -1,16 +1,12 @@
 const ap = new APlayer({
-    container: document.getElementById('player'),
-    listFolded: false,//�б�Ĭ���۵�
-    listMaxHeight: 90,//�б����߶�
+    container: document.getElementById('aplayer'),
     autoplay: true,
-    audio: [
-        {
+    audio: [{
             name: 'Waterwheel',
             artist: 'ZuJian',
             url: 'http://music.163.com/song/media/outer/url?id=28409319.mp3',
             cover: 'https://p2.music.126.net/4IxuKCkzOj3uW6to5sl29A==/109951164021533111.jpg'
-        },
-    ]
+        }]
 });
     
 
