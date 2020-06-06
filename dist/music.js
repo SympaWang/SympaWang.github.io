6c5a42b88fb26d6b380eff5993b50edfcbe817b4
Your Name <you@example.com>
2020-06-06 17:47:16 +0800

    Site updated: 2020-06-06 17:47:15

--- dist/music.js
+++ dist/music.js
@@ -1,8 +1,8 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     audio: [{
-        name: 'ˮ܇���{��',
-        artist: '�������',
+        name: 'Waterwheel',
+        artist: 'ZuJian',
         url: 'http://music.163.com/song/media/outer/url?id=28409319.mp3',
         cover: 'https://p2.music.126.net/4IxuKCkzOj3uW6to5sl29A==/109951164021533111.jpg'
     }]
