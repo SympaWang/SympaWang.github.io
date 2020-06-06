3d6fae6fd36b32a38a29511f5e7c9eeeff5d62b3
Your Name <you@example.com>
2020-06-06 16:38:11 +0800

    Site updated: 2020-06-06 16:38:11

--- dist/music.js
+++ dist/music.js
@@ -9,7 +9,7 @@ const ap = new APlayer({
         {
             name: 'Euterpe',
             artist: 'EGOIST',
-            url: '722932',
+            url: 'https://music.163.com/outchain/player?type=2&id=722932&auto=1&height=66',
             cover: 'cover1.jpg',
         },
         {
