8eaf9709e47cf822083fc5ff7981032fafe81977
Your Name <you@example.com>
2020-06-06 16:48:44 +0800

    Site updated: 2020-06-06 16:48:44

--- dist/music.js
+++ dist/music.js
@@ -9,7 +9,7 @@ const ap = new APlayer({
         {
             name: 'Euterpe',
             artist: 'EGOIST',
-            url: 'https://music.163.com/outchain/player?type=2&id=722932&auto=1&height=66',
+            url: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=722932&auto=1&height=66"></iframe>',
             cover: 'cover1.jpg',
         },
         {
